cce2961826be7a0d0b9b21501b7463e89af0ae8b
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-12-11 00:59:09 +0200

    Pug impl

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const path = require("path");
-
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "views");
+const users = [];
 
-const form = require("./routes/form");
-const users = require("./routes/users");
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(form.routes);
-app.use(users);
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req, res, next) => {
+  res.render('index', { pageTitle: 'Add User' });
+});
 
-app.use((req, res, next) => {
-    res.status(404).render("404", {pageTitle: "Page not found."})
+app.get('/users', (req, res, next) => {
+  res.render('users', { pageTitle: 'User', users: users });
+});
+
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect('/users');
 });
 
 app.listen(3000);
