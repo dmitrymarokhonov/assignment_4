17464bbc70781019807b1e88a967074c2d95d7bb
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-12-11 10:11:46 +0200

    EJS impl

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 
 const users = [];
 
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
